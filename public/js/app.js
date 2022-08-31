cbbf524de0a345eff494965661fc6f69aff3cdd7
Muhammad Al Ghifari <muhamadalghifari97@gmail.com>
2022-08-31 14:48:31 +0700

    change abbviate number

--- public/js/app.js
+++ public/js/app.js
@@ -22292,25 +22292,21 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 function abbreviateNumber(value) {
-  var newValue = value;
-
-  if (value >= 1000) {
-    var suffixes = ["", "k", "m", "b", "t"];
-    var suffixNum = Math.floor(("" + value).length / 3);
-    var shortValue = '';
-
-    for (var precision = 2; precision >= 1; precision--) {
-      shortValue = parseFloat((suffixNum != 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(precision));
-      var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
-
-      if (dotLessShortValue.length <= 2) {
-        break;
-      }
-    }
-
-    if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1);
-    newValue = shortValue + suffixes[suffixNum];
-  }
+  var newValue = Intl.NumberFormat('en-US', {
+    notation: "compact",
+    maximumFractionDigits: 1
+  }).format(value); // if (value >= 1000) {
+  //     var suffixes = ["", "k", "m", "b","t"];
+  //     var suffixNum = Math.floor( (""+value).length/3 );
+  //     var shortValue = '';
+  //     for (var precision = 2; precision >= 1; precision--) {
+  //         shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+  //         var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+  //         if (dotLessShortValue.length <= 2) { break; }
+  //     }
+  //     if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+  //     newValue = shortValue+suffixes[suffixNum];
+  // }
 
   return newValue;
 }
